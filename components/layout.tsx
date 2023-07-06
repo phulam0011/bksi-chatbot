19086742c60297561b5788c4cf63c9641d89ed69
phulam0011 <phulam0011@gmail.com>
2023-07-06 20:27:53 +0700

    first commit

---
+++ components/layout.tsx
@@ -0,0 +1,26 @@
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="mx-auto flex flex-col space-y-4">
+      {/* <header className="container sticky top-0 z-40 bg-white">
+        <div className="h-16 border-b border-b-slate-200 py-4">
+          <nav className="navMenu">
+            <a href="#">Home</a>
+            <a href="#">Blog</a>
+            <a href="#">Work</a>
+            <a href="#">About</a>
+            <div className="dot"></div>
+          </nav>
+        </div>
+      </header> */}
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
